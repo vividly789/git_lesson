48a045e5657fe6586eb16fcc47ca66d2732eb050
Niraj Gurung <57787864+vividly789@users.noreply.github.com>
2019-11-15 14:45:52 -0800

    write documentation for my_square
    uses python's ** operator

--- src/my_square.py
+++ src/my_square.py
@@ -1,5 +1,5 @@
 def my_square(x):
-         """takes a value and return the squared value.
+         """takes a value and return  the squared value.
          """
          return(x ** 2)
 print(my_square(3))
